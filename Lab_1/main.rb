a53848f38892571c0a2821c9932a6e29b0df6287
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-11-10 18:58:02 +0200

    Changed output lab1

--- Lab_1/main.rb
+++ Lab_1/main.rb
@@ -3,11 +3,11 @@
 include Math
 
 def func(x, t, fi)
-  tan((exp(t) + 3**fi) / sqrt((t**2 + 2).abs)) - ((cos(fi)**3 + 2.8 * 10**-3.4 + x) / (sin(PI - 4) + 1.2)**(1 / 5))
+  tan((exp(t) + 3**fi) / sqrt((t**2 + 2).abs)) - ((cos(fi)**3 + 2.8 * 10**-3.4 + x) / (sin(PI - 4)**3 + 1.2)**(1 / 5))
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts 'Hello World!'
+  puts ''
   z = func(2, 3, 4)
   puts z
 end
